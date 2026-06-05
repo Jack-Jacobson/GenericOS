fcdbf5ded49b24e199b9050c11d43d2cc2758935
jackjacobson <jackjacobson.co@gmail.com>
2026-06-04 22:49:23 -0600

    finished up terminal

--- commands.js
+++ commands.js
@@ -1,3 +1,14 @@
 const commands = {
-    test: "test recieved :D"
+    test: "test recieved :D",
+    whoami: "you are you, obviously",
+    version: "v0.0.0.0 HOW ARE YOU SEEING THIS, I HAVENT RELEASED THIS YET D",
+    selfDestruct: "Run. Self destructing in 3...",
+    gaslight: "Error 404, gaslight not a real command",
+    status: "If you are reading this, terminal working ig?",
+    ping: "Pong",
+    hi: "Hi there! You liking GenericOS?",
+    hello: "Hi there! You liking GenericOS?",
+    bye: "Don't leave me pwease 👉️👈️",
+    goodbye: "Dont leave me pwease 👉️👈️",
+    help: "The following are usable commands, youll have to use them yourself to figure out what they do. COMMANDS ARE CASE SENSITIVE\n\ntest\nwhoami\nversion\nselfDestruct\ngaslight\nstatus\nping\nhi\nhello\nbye\ngoodbye"
 };
